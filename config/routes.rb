97281bd852ddb79a6633c80f222812fe5dda4e90
Have Samuel <samiehave9@gmail.com>
2022-07-05 20:59:15 +0300

    Added atable

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
-get '/greeting', to: 'custom_pages#hello'
+# get '/greeting', to: 'custom_pages#hello'
