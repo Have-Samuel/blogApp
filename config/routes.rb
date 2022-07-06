defb8820abf9982010aaec0ac65ec8d09c5e1c20
Have Samuel <samiehave9@gmail.com>
2022-07-06 12:07:15 +0300

    Edited the files and installed the dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-# get '/greeting', to: 'custom_pages#hello'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
