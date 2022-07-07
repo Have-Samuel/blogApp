6714650fc9648ae6671344f502f7d25f3a790bc6
Have Samuel <samiehave9@gmail.com>
2022-07-08 01:11:54 +0300

    Adding conditional commands in the comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'author_id'
+  validates :text, presence: true length: { minimum:3, maximum: 255 }
 
   def update_comments_conuter
+    post.comments_counter = 0 if post.comments_counter.nil?
     post.increment!(:comments_counter)
   end
 end
