dd29c6151b9cef1d0bf4c37fe63ad5988cb24ebc
Have Samuel <samiehave9@gmail.com>
2022-07-08 01:23:09 +0300

    Adding conditional commands in the post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user, foreign_key: :author_id
   has_many :likes
   has_many :comments
+  validates :title, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :text, length: { minimum: 3, maximum: 255 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_5_comments
     comments.order!(created_at: :desc).limit(5)
@@ -11,6 +15,7 @@ class Post < ActiveRecord::Base
   private
 
   def update_post_counter
+    user.post_counter = o if user.post_counter.nil?
     user.increment!(:post_counter)
   end
 end
