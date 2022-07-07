faabba922af7bdac5fc21c28970857931967fd00
Have Samuel <samiehave9@gmail.com>
2022-07-07 03:12:45 +0300

    Added customized methods tha display in the console

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :post
-  has_many :likes
+  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :post, foreign_key: 'author_id'
+
+  def update_comments_conuter
+    post.increment!(:comments_counter)
+  end
 end
+
+post = Post.new(comments_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, foreign_key: :author_id
   has_many :likes
   has_many :comments
+
+  def update_post_conuter
+    user.increment!(:post_counter)
+  end
+
+  def last_5_comments
+    comments.order!(created_at: :desc).limit(5)
+  end
 end
+
+Dan = User.select(:id).find_by(name: 'Dan')
+post = Post.first
